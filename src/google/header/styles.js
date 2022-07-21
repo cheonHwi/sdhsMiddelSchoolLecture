3bb00df86e8ce7b00e71243846d18d54059cb7b4
dhwiwwx <sdh210320@sdh.hs.kr>
2022-07-21 15:56:04 +0900

    finish google page component

--- src/google/header/styles.js
+++ src/google/header/styles.js
@@ -3,9 +3,34 @@ import styled from '@emotion/styled'
 export const GogleHeader = styled.header`
     width: 100vw;
     height: 10vh;
-    background-color: antiquewhite;
+    background: #fff;
     display: flex;
     justify-content: space-between;
     align-items: center;
 
+`;
+export const Goglenav1 = styled.div`
+  padding:0 10px;
+&>a{
+    padding-left:20px;
+}
+`;
+export const Goglenav2 = styled.div`
+    width:200px;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    & a{
+        font-size:15px;
+    }
+    &>img{
+        width:32px;
+        height:32px;
+        border-radius:50%;
+    }
+`;
+export const Gb_ue = styled.svg`
+    width:22px;
+    height:22px;
+}
 `;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import SamsungHeader from "./samsung/header";
-import SamsungFooter from "./samsung/footer";
-import SamsungSection from "./samsung/section";
+import GoogleHeader from "./google/header";
+import GoogleSection from "./google/section";
+import GoogleFooter from "./google/footer";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <SamsungHeader />
-    <SamsungSection />
-    <SamsungFooter />
+    <GoogleHeader />
+    <GoogleSection />
+    <GoogleFooter />
   </>
 );
 
